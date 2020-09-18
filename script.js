6d97224a5409bb92d0708aff828fa1cd56f4b372
kousuke-bass <70252775+kousuke-bass@users.noreply.github.com>
2020-09-18 21:16:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 $(function(){
+  let topics =$('.topics').offset().top;
+  let institution =$('.institution').offset().top;
+  let recruit =$('.recruit').offset().top;
+  let contact =$('.contact').offset().top;
 
-
-$('.hamburger').click(function(){
-
+//ハンバーガーメニュー
+$('.nav-button').on('click',function(){
+$('.nav-button').toggleClass('is-active');
+let button =$('.nav-button').hasClass('is-active');
+if(button){
   $('.menu').slideDown();
+}else{
+  $('.menu').slideUp();
+}
 });
 
-$('.menu a').click(function(){
-  $('.menu').slideUp();
+$('.menu-list li').on('click',function(){
+  let click=$(this).attr('id');
+  console.log(click);
+
+  if(click=="topics"){
+    $(window).scrollTop(topics);
+    $('.menu').slideUp();
+    $('.nav-button').removeClass('is-active');
+  }
+  else if (click=="institution") {
+    $(window).scrollTop(institution);
+    $('.menu').slideUp();
+    $('.nav-button').removeClass('is-active');
+
+
+  }
+  else if (click=="recruit") {
+    $(window).scrollTop(recruit);
+    $('.menu').slideUp();
+    $('.nav-button').removeClass('is-active');
+
+
+  }
+  else if (click=="contact") {
+    $(window).scrollTop(contact);
+    $('.menu').slideUp();
+    $('.nav-button').removeClass('is-active');
+
+
+  }
 });
 
 });
